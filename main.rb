0063e19f78323d0096ef900fcbb541fda7b766dc
NickyH <nickyhughes00@gmail.com>
2013-02-04 18:35:47 -0500

    Added main.rb to dev branch

---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require 'pg'
+require 'httparty'
+require 'JSON'
+
+
+conn = PG.connect(:dbname =>'moviez', :host => 'localhost')
+conn.exec( "select * from moviez" ) do |result|
+  result.each do |row|
+
+  end
+end
+
+print 'Enter a movie name: '
+title = gets.chomp
+
+movie = HTTParty.get('http://www.omdbapi.com/?t=#{title.split.join('+')}')
+
+movie_formatted = JSON(movie.body)
+
+sql = "insert into people (name) values (movie_formatted)"
+conn.exec(sql)
+
+conn.close
